dfd55b7bf352f8c5ede611617e2ce9997bc5ab77
Grot710 <gr00tisbest@gmail.com>
2025-10-21 09:35:27 -0700

    started background

--- assets/js/mansionGame/mansionLevel5.js
+++ assets/js/mansionGame/mansionLevel5.js
@@ -1,58 +1,44 @@
-import GameEnvBackground from './GameEngine/<path>.js'
-import AnimatedPlayer from './GameEngine/<path>.js'
-import InteractiveObject from './GameEngine/<path>.js'
-import GameEngine from './GameEngine/<path>.js'
-import SoundManager from './GameEngine/<path>.js'
-import LevelManager from './GameEngine/<path>.js'
+import GameEnvBackground from './GameEngine/GameEnvBackground.js'
+import AnimatedPlayer from './GameEngine/AnimatedPlayer.js'
 
-const mansionLevel5 = () => {
-    const gameEnv = new GameEnvBackground({
-        backgroundImage: 'assets/images/mansion/level5/background.png',
-        width: 800,
-        height: 600
-    });
-
-    const player = new AnimatedPlayer({
-        spriteSheet: 'assets/images/characters/player.png',
-        frameWidth: 64,
-        frameHeight: 64,
-        animations: {
-            idle: [0, 1, 2, 3],
-            walk: [4, 5, 6, 7]
-        },
-        initialPosition: { x: 100, y: 500 }
-    });
-
-    const interactiveObjects = [
-        new InteractiveObject({
-            image: 'assets/images/mansion/level5/object1.png',
-            position: { x: 300, y: 450 },
-            onInteract: () => {
-                SoundManager.playSound('assets/sounds/mansion/level5/object1_interact.mp3');
-                alert('You found a hidden clue!');
-            }
-        }),
-        new InteractiveObject({
-            image: 'assets/images/mansion/level5/object2.png',
-            position: { x: 600, y: 400 },
-            onInteract: () => {
-                SoundManager.playSound('assets/sounds/mansion/level5/object2_interact.mp3');
-                alert('You unlocked a secret passage!');
-            }
-        })
-    ];
-
-    const levelConfig = {
-        gameEnv,
-        player,
-        interactiveObjects,
-        levelNumber: 5,
-        nextLevel: 'mansionLevel6'
+class GameLevelMansion5 {
+    constructor(gameEnv) {
+        let width = gameEnv.innerWidth;
+        let height = gameEnv.innerHeight;
+        let path = gameEnv.path;
+        const image_src_library = path + "/images/gamify/libraryfloor.png";
+        const image_data_library = {
+        id: 'Library',
+        src: image_src_library,
+        pixels: {height: 597, width: 340}
     };
-
-    LevelManager.registerLevel(levelConfig);
-
-    return new GameEngine(levelConfig);
-};
-
-export default mansionLevel5;
+    const sprite_src_mc = path + "/images/gamify/octopus.png"; // be sure to include the path
+    const MC_SCALE_FACTOR = 5;
+    const sprite_data_mc = {
+        id: 'Octopus',
+        greeting: "Hi I am Octopus, the water wanderer. I am looking for wisdome and adventure!",
+        src: sprite_src_octopus,
+        SCALE_FACTOR: MC_SCALE_FACTOR,
+        STEP_FACTOR: 1000,
+        ANIMATION_RATE: 50,
+        GRAVITY: true,
+        INIT_POSITION: { x: 0, y: height - (height/MC_SCALE_FACTOR) }, 
+        pixels: {height: 250, width: 167},
+        orientation: {rows: 3, columns: 2 },
+        down: {row: 0, start: 0, columns: 2 },
+        downLeft: {row: 0, start: 0, columns: 2, mirror: true, rotate: Math.PI/16 }, // mirror is used to flip the sprite
+        downRight: {row: 0, start: 0, columns: 2, rotate: -Math.PI/16 },
+        left: {row: 1, start: 0, columns: 2, mirror: true }, // mirror is used to flip the sprite
+        right: {row: 1, start: 0, columns: 2 },
+        up: {row: 0, start: 0, columns: 2},
+        upLeft: {row: 1, start: 0, columns: 2, mirror: true, rotate: -Math.Pi/16 }, // mirror is used to flip the sprite
+        upRight: {row: 1, start: 0, columns: 2, rotate: Math.PI/16 },
+        hitbox: { widthPercentage: 0.45, heightPercentage: 0.2 },
+        keypress: { up: 87, left: 65, down: 83, right: 68 } // W, A, S, D
+    };
+    this.classes = [      
+      {class: GameEnvBackground, data: image_data_library },
+      {class: AnimatedPlayer, data: sprite_data_mc },
+    ];
+    }
+}
